e4baf7ce10482a7428e1398da84ab4515f741c81
Jordan Byron <jordan.byron@seeclickfix.com>
2018-03-08 20:18:06 -0500

    First round of specs

---
+++ spec/lib/depbot/gem_change_spec.rb
@@ -0,0 +1,177 @@
+require 'depbot/gem_change'
+
+RSpec.describe Depbot::GemChange do
+  let(:name) { 'depbot' }
+  let(:previous_version) { '0.0.0' }
+  let(:current_version) { '0.0.1' }
+
+  let(:github_url) { 'https://github.com/seeclickfix/depbot' }
+  let(:changelog_url) { "#{github_url}/CHANGELOG.md" }
+
+  subject { described_class.new(name, previous_version, current_version) }
+
+  context 'rubygems info' do
+    context 'when rubygems.org is unreachable or errors' do
+      it 'fails gracefully'
+    end
+
+    describe '#homepage_url' do
+      let(:info_from_rubygems) { { 'homepage_uri' => homepage_uri } }
+
+      context 'when homepage_uri is present' do
+        let(:homepage_uri) { github_url }
+
+        it 'returns the result' do
+          allow(subject).to receive(:rubygems_info)
+            .and_return(info_from_rubygems)
+
+          expect(subject.homepage_url).to eq(homepage_uri)
+        end
+      end
+
+      context 'when the homepage_uri is an empty string' do
+        let(:homepage_uri) { '' }
+
+        it 'returns nil' do
+          allow(subject).to receive(:rubygems_info)
+            .and_return(info_from_rubygems)
+
+          expect(subject.homepage_url).to eq(nil)
+        end
+      end
+    end
+
+    describe '#source_code_url' do
+      let(:info_from_rubygems) { { 'source_code_uri' => source_code_uri } }
+
+      context 'when source_code_uri is present' do
+        let(:source_code_uri) { github_url }
+
+        it 'returns the result' do
+          allow(subject).to receive(:rubygems_info)
+            .and_return(info_from_rubygems)
+
+          expect(subject.source_code_url).to eq(source_code_uri)
+        end
+      end
+
+      context 'when the source_code_uri is an empty string' do
+        let(:source_code_uri) { '' }
+
+        it 'returns nil' do
+          allow(subject).to receive(:rubygems_info)
+            .and_return(info_from_rubygems)
+
+          expect(subject.source_code_url).to eq(nil)
+        end
+      end
+    end
+  end
+
+  describe '#github_url' do
+    context 'with no homepage_url or source_code_url' do
+      it 'returns nil' do
+        allow(subject).to receive(:homepage_url).and_return(nil)
+        allow(subject).to receive(:source_code_url).and_return(nil)
+
+        expect(subject.github_url).to eq(nil)
+      end
+    end
+
+    context 'with a homepage_url and source_code_url not on github' do
+      let(:non_github_url) { 'http://bitbucket.com/seeclickfix/depbot' }
+
+      it 'returns nil' do
+        allow(subject).to receive(:homepage_url).and_return(non_github_url)
+        allow(subject).to receive(:source_code_url).and_return(non_github_url)
+
+        expect(subject.github_url).to eq(nil)
+      end
+    end
+
+    context 'with a homepage_url or source_code_url on github' do
+      it 'returns the first matched github url' do
+        allow(subject).to receive(:homepage_url).and_return(github_url)
+        allow(subject).to receive(:source_code_url).and_return(github_url)
+
+        expect(subject.github_url).to eq(github_url)
+      end
+    end
+  end
+
+  describe '#changelog_url' do
+    it 'delegates to Github::Changelog.for' do
+      allow(subject).to receive(:github_url).and_return(github_url)
+      allow(Depbot::Github::Changelog).to receive(:for)
+        .and_return(changelog_url)
+
+      expect(subject.changelog_url).to eq(changelog_url)
+      expect(Depbot::Github::Changelog).to have_received(:for)
+        .with('seeclickfix/depbot', 'v0.0.1')
+    end
+  end
+
+  describe '#compare_url' do
+    context 'without a github_url' do
+      it 'returns nil' do
+        allow(subject).to receive(:github_url).and_return(nil)
+
+        expect(subject.compare_url).to eq(nil)
+      end
+    end
+
+    context 'with a github_url' do
+      it 'returns a compare url for the two versions' do
+        allow(subject).to receive(:github_url).and_return(github_url)
+
+        expect(subject.compare_url).to include(github_url)
+          .and include('compare')
+          .and include(previous_version)
+          .and include(current_version)
+      end
+    end
+  end
+
+  describe '#to_markdown' do
+    context 'with no github_url' do
+      it 'returns the gem name and version change information' do
+        allow(subject).to receive(:github_url).and_return(nil)
+
+        expect(subject.to_markdown).to include(name)
+          .and include(previous_version)
+          .and include(current_version)
+      end
+    end
+
+    context 'with a github_url' do
+      before do
+        allow(subject).to receive(:github_url).and_return(github_url)
+        allow(subject).to receive(:changelog_url).and_return(nil)
+      end
+
+      it 'returns valid markdown markup'
+
+      it 'returns the gem name and version change information' do
+        expect(subject.to_markdown).to include(name)
+          .and include(previous_version)
+          .and include(current_version)
+      end
+
+      it 'returns a link to github' do
+        expect(subject.to_markdown).to include(github_url)
+      end
+
+      it 'returns a link to the version comparison' do
+        expect(subject.to_markdown).to include(subject.compare_url)
+      end
+
+      context 'with a changelog' do
+        it 'returns a link to the changelog' do
+          allow(subject).to receive(:changelog_url).and_return(changelog_url)
+
+          expect(subject.to_markdown).to include(changelog_url)
+        end
+      end
+    end
+  end
+end
