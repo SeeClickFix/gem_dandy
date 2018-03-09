af742454354e4a2c565f661669643a8404327e6e
Jordan Byron <jordan.byron@seeclickfix.com>
2018-03-09 13:30:04 -0500

    YOLO :robot_face:

--- lib/depbot/github.rb
+++ lib/depbot/github.rb
@@ -3,7 +3,7 @@ require "octokit"
 module Depbot
   module Github
     URL = 'https://github.com'.freeze
-    GIT_URL = 'git@github.com:'.freeze
+    GIT_URL = 'git@github.com'.freeze
 
     def self.client
       @client ||= Octokit::Client.new(access_token: ENV["GITHUB_ACCESS_TOKEN"])
