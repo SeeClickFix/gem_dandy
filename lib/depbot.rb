9f31ad6be531b6e5b8237d8a7f0c7fb1c666a392
Jordan Byron <jordan.byron@seeclickfix.com>
2018-03-16 14:12:37 -0400

    Add Depbot::Env for temporarily setting bundler ENV vars

--- lib/depbot.rb
+++ lib/depbot.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
 require_relative './depbot/github'
 require_relative './depbot/github/changelog'
 require_relative './depbot/git_repo'
 require_relative './depbot/gem_change'
 require_relative './depbot/diff_parser'
+require_relative './depbot/env'
 
 module Depbot
-  GITHUB_URL = 'https://github.com/SeeClickFix/depbot'.freeze
+  GITHUB_URL = 'https://github.com/SeeClickFix/depbot'
   PULL_REQUEST_FOOTER = "\n\n--\n\n" \
     "Brought to you by [depbot](#{GITHUB_URL}) - Automated Gemfile Updates\n" \
     "<sub>Feedback or Bug Reports? File a [ticket](#{GITHUB_URL}/issues).</sub>"
