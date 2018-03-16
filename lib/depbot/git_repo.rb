f8efed2f3f71b15f37f57e6285805448cbbd0651
Jordan Byron <jordan.byron@seeclickfix.com>
2018-03-16 14:24:34 -0400

    Allow git user name / email to be set
    For Heroku!

--- lib/depbot/git_repo.rb
+++ lib/depbot/git_repo.rb
@@ -11,12 +11,6 @@ module Depbot
       @update_branch = "bundle-update-#{Time.now.strftime('%Y-%m-%d-%H%M%S')}"
       @path = File.join(TMP_PATH, repo)
 
-      @git = if Dir.exist?(path)
-               Git.open(path)
-             else
-               Git.clone(url, repo, path: TMP_PATH)
-             end
-
       reset_remote
     end
 
@@ -47,14 +41,30 @@ module Depbot
 
     private
 
-    attr_reader :git
-
     def reset_remote
       git.reset_hard
       git.checkout(base_branch)
       delete_update_branch
       git.fetch
       git.reset_hard('@{u}')
+    end
+
+    def git
+      @git ||= begin
+        if Dir.exist?(path)
+          Git.open(path)
+        else
+          Git.clone(url, repo, path: TMP_PATH)
+        end.tap do |g|
+          if (name = ENV['GIT_USER_NAME'])
+            g.config('user.name', name)
+          end
+
+          if (email = ENV['GIT_USER_EMAIL'])
+            g.config('user.email', email)
+          end
+        end
+      end
     end
   end
 end
