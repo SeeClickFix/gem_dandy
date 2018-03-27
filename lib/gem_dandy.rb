be71ecd71b022fa6b2a3b38d6653018672ca14c0
Jordan Byron <jordan.byron@seeclickfix.com>
2018-03-27 11:42:47 -0400

    Add a version file. For version numbers.

--- lib/gem_dandy.rb
+++ lib/gem_dandy.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative './gem_dandy/version'
 require_relative './gem_dandy/github'
 require_relative './gem_dandy/github/changelog'
 require_relative './gem_dandy/git_repo'
